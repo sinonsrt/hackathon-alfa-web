b2824ecc49c3269176398cbb7ef23fbe87f2c55b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-08 20:59:37 -0300

    feat: add estilo css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- style -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>CHERRY Veículos</title>
+</head>
+
+<body>
+
+    <!-- HEADER -->
+    <header class="main-navbar">
+        <div>
+            <a href="{{ url('/') }}" class="logo-text">CHERRY Veículos</a>
+        </div>
+        <nav class="link-navbar">
+            <ul>
+                <li>HOME</li>
+                <li>NOVOS</li>
+                <li>USADOS</li>
+            </ul>
+        </nav>
+    </header>
+
+
+</body>
+
+</html>
