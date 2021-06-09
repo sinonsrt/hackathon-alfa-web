7ecda83b4bfc729d54a1b8fae710e4c647c79586
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-09 20:23:26 -0300

    formataçao pagina home

---
+++ resources/views/home-bkp.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- style -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>CHERRY Veículos</title>
+</head>
+
+<body>
+
+    <!-- HEADER -->
+    <!-- <header class="main-navbar">
+        <div>
+            <a href="{{ url('/') }}" class="logo-text">CHERRY Veículos</a>
+        </div>
+        <nav class="link-navbar">
+            <ul>
+                <li>HOME</li>
+                <li>NOVOS</li>
+                <li>USADOS</li>
+            </ul>
+        </nav>
+    </header> -->
+
+    <!-- As a link -->
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+    </nav>
+
+    <!-- As a heading -->
+    <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+
+
+</body>
+
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,223 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
+<!doctype html>
+<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- style -->
-    <link href="css/style.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
 
     <title>CHERRY Veículos</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/4.6/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/4.6/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/4.6/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/4.6/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/4.6/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+    <link rel="icon" href="/docs/4.6/assets/img/favicons/favicon.ico">
+    <meta name="msapplication-config" content="/docs/4.6/assets/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#563d7c">
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
 </head>
 
 <body>
 
-    <!-- HEADER -->
-    <header class="main-navbar">
-        <div>
-            <a href="{{ url('/') }}" class="logo-text">CHERRY Veículos</a>
+    <header>
+        <div class="collapse bg-dark" id="navbarHeader">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 text-white col-md-7 py-4">
+                        <h4>Sobre Nós</h4>
+                        <p>Referência em Umuarama no ramo de veículos novos e seminovos a Cherry trabalha com as principais marcas do brasil.
+                            A Cherry Veículos vem conquistando ainda mais o público de Umuarama com excelente e personalizado atendimento e também, claro, com a garantia que você procura. Venha conferir.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 offset-md-1 py-4">
+                        <h4 class="text-white">Contato</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Fone: (44)3622-9999</a></li>
+                            <li><a href="#" class="text-white">E-mail: cherry@jacford.com.br</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-        <nav class="link-navbar">
-            <ul>
-                <li>HOME</li>
-                <li>NOVOS</li>
-                <li>USADOS</li>
-            </ul>
-        </nav>
+        <div class="navbar main-navbar">
+            <div class="container d-flex justify-content-between">
+                <a href="/" class="navbar-brand logo-text d-flex align-items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false">
+                        <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+                        <circle cx="12" cy="13" r="4" />
+                    </svg>
+                    <strong>CHERRY</strong>
+                </a>
+
+                <nav class="nav">
+                    <a class="nav-link" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation" href="#">INSTITUCIONAL</a>
+                    <a class="nav-link" href="#">NOVOS</a>
+                    <a class="nav-link" href="#">SEMINOVOS</a>
+                </nav>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
     </header>
+
+    <main role="main">
+
+        <section class="jumbotron mb-0">
+            <div class="container w-50">
+                <h3>Pesquisa</h3>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control " placeholder="Modelo ou marca...">
+                    <div class="input-group-prepend">
+                        <button class="btn btn-secondary" type="button" id="button-addon1">Buscar</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="py-5 bg-light">
+            <div class="container">
+
+                <h2 class="mb-5 text-center">Destaques</h2>
+
+                <div class="row">
+
+                    <div class="col-md-3">
+                        <div class="veiculo-card-container">
+                            <h3 class="veiculo-card-title">
+                                GOL G6 1.0 FLEX 2P
+                            </h3>
+
+                            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+                            <h3 class="veiculo-card-price">
+                                R$ 35.000,00
+                            </h3>
+
+                            <div class="veiculo-card-description">
+                                <p>
+                                    <b>Marca:</b> Volksvagen <br>
+                                    <b>Cor:</b> Branca <br>
+                                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="veiculo-card-container">
+                            <h3 class="veiculo-card-title">
+                                GOL G6 1.0 FLEX 2P
+                            </h3>
+
+                            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+                            <h3 class="veiculo-card-price">
+                                R$ 35.000,00
+                            </h3>
+
+                            <div class="veiculo-card-description">
+                                <p>
+                                    <b>Marca:</b> Volksvagen <br>
+                                    <b>Cor:</b> Branca <br>
+                                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="veiculo-card-container">
+                            <h3 class="veiculo-card-title">
+                                GOL G6 1.0 FLEX 2P
+                            </h3>
+
+                            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+                            <h3 class="veiculo-card-price">
+                                R$ 35.000,00
+                            </h3>
+
+                            <div class="veiculo-card-description">
+                                <p>
+                                    <b>Marca:</b> Volksvagen <br>
+                                    <b>Cor:</b> Branca <br>
+                                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="veiculo-card-container">
+                            <h3 class="veiculo-card-title">
+                                GOL G6 1.0 FLEX 2P
+                            </h3>
+
+                            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+                            <h3 class="veiculo-card-price">
+                                R$ 35.000,00
+                            </h3>
+
+                            <div class="veiculo-card-description">
+                                <p>
+                                    <b>Marca:</b> Volksvagen <br>
+                                    <b>Cor:</b> Branca <br>
+                                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+    <footer class="bg-dark">
+        <div class="container py-4 text-white text-center">
+            Desenvolvido por Carlos Ferreira | Lucas Marchiori | Ayslan Fernandes
+
+            <div class="footer-icons">
+
+            </div>
+        </div>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
 
 </body>
