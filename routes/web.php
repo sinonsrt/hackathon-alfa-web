673abefbfa97e32a58b447642c9ed7ef5204295c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-09 22:13:40 -0300

    adiciona pagina seminovos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NovosController;
+use App\Http\Controllers\SeminovosController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,3 +22,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/novos', [NovosController::class, 'index']);
+Route::get('/seminovos', [SeminovosController::class, 'index']);
