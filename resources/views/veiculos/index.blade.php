b6e8501a774b6e2dfef27170e405f046f13c1662
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-09 21:21:41 -0300

    adiciona template

---
+++ resources/views/veiculos/index.blade.php
@@ -0,0 +1,99 @@
+@extends(layout.template)
+
+@section('destaques')
+
+<h2 class="mb-5 text-center">Destaques</h2>
+
+<div class="row">
+
+    <div class="col-md-3">
+        <div class="veiculo-card-container">
+            <h3 class="veiculo-card-title">
+                GOL G6 1.0 FLEX 2P
+            </h3>
+
+            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+            <h3 class="veiculo-card-price">
+                R$ 35.000,00
+            </h3>
+
+            <div class="veiculo-card-description">
+                <p>
+                    <b>Marca:</b> Volksvagen <br>
+                    <b>Cor:</b> Branca <br>
+                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="veiculo-card-container">
+            <h3 class="veiculo-card-title">
+                GOL G6 1.0 FLEX 2P
+            </h3>
+
+            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+            <h3 class="veiculo-card-price">
+                R$ 35.000,00
+            </h3>
+
+            <div class="veiculo-card-description">
+                <p>
+                    <b>Marca:</b> Volksvagen <br>
+                    <b>Cor:</b> Branca <br>
+                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="veiculo-card-container">
+            <h3 class="veiculo-card-title">
+                GOL G6 1.0 FLEX 2P
+            </h3>
+
+            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+            <h3 class="veiculo-card-price">
+                R$ 35.000,00
+            </h3>
+
+            <div class="veiculo-card-description">
+                <p>
+                    <b>Marca:</b> Volksvagen <br>
+                    <b>Cor:</b> Branca <br>
+                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="veiculo-card-container">
+            <h3 class="veiculo-card-title">
+                GOL G6 1.0 FLEX 2P
+            </h3>
+
+            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+
+            <h3 class="veiculo-card-price">
+                R$ 35.000,00
+            </h3>
+
+            <div class="veiculo-card-description">
+                <p>
+                    <b>Marca:</b> Volksvagen <br>
+                    <b>Cor:</b> Branca <br>
+                    <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
+                </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@endsection
