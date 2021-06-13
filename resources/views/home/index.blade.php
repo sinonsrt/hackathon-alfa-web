86361ab1eb9fb86e1f752e0470619b2c4a7537a6
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-13 09:44:51 -0300

    style: ajuste css

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,19 +9,19 @@
     <div class="col-md-3">
         <div class="veiculo-card-container">
             <h3 class="veiculo-card-title">
-                GOL G6 1.0 FLEX 2P
+                GOLF 1.8 4P
             </h3>
 
-            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+            <img src="https://hackaton-img.s3.us-east-2.amazonaws.com/1623524727214_12057.jpeg" class="veiculo-card-image" />
 
             <h3 class="veiculo-card-price">
-                R$ 35.000,00
+                R$ 72.900,00
             </h3>
 
             <div class="veiculo-card-description">
                 <p>
                     <b>Marca:</b> Volksvagen <br>
-                    <b>Cor:</b> Branca <br>
+                    <b>Cor:</b> Branco <br>
                     <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
                 </p>
             </div>
@@ -31,19 +31,19 @@
     <div class="col-md-3">
         <div class="veiculo-card-container">
             <h3 class="veiculo-card-title">
-                GOL G6 1.0 FLEX 2P
+                CRUZE 2020 2.0 4P
             </h3>
 
-            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+            <img src="https://hackaton-img.s3.us-east-2.amazonaws.com/1623523767618_13069.jpeg" class="veiculo-card-image" />
 
             <h3 class="veiculo-card-price">
-                R$ 35.000,00
+                R$ 99.900,00
             </h3>
 
             <div class="veiculo-card-description">
                 <p>
-                    <b>Marca:</b> Volksvagen <br>
-                    <b>Cor:</b> Branca <br>
+                    <b>Marca:</b> Chevrolet <br>
+                    <b>Cor:</b> Preto <br>
                     <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
                 </p>
             </div>
@@ -53,19 +53,19 @@
     <div class="col-md-3">
         <div class="veiculo-card-container">
             <h3 class="veiculo-card-title">
-                GOL G6 1.0 FLEX 2P
+                FUSION 2.0 FLEX 4P
             </h3>
 
-            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+            <img src="https://hackaton-img.s3.us-east-2.amazonaws.com/1623530268366_16570.jpeg" class="veiculo-card-image" />
 
             <h3 class="veiculo-card-price">
-                R$ 35.000,00
+                R$ 109.900,00
             </h3>
 
             <div class="veiculo-card-description">
                 <p>
-                    <b>Marca:</b> Volksvagen <br>
-                    <b>Cor:</b> Branca <br>
+                    <b>Marca:</b> Ford <br>
+                    <b>Cor:</b> Prata <br>
                     <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
                 </p>
             </div>
@@ -75,19 +75,19 @@
     <div class="col-md-3">
         <div class="veiculo-card-container">
             <h3 class="veiculo-card-title">
-                GOL G6 1.0 FLEX 2P
+                JETTA 2.0 FLEX 2P
             </h3>
 
-            <img src="https:via.placeholder.com/150" class="veiculo-card-image" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/cherry-veiculos.appspot.com/o/jetta_001.jpeg?alt=media&token=ca0823ec-7b3d-4a7e-970b-64ee61a430cb" class="veiculo-card-image" />
 
             <h3 class="veiculo-card-price">
-                R$ 35.000,00
+                R$ 134.900,00
             </h3>
 
             <div class="veiculo-card-description">
                 <p>
                     <b>Marca:</b> Volksvagen <br>
-                    <b>Cor:</b> Branca <br>
+                    <b>Cor:</b> Prata <br>
                     <b>Opcionais:</b> vidro elétrico, direção hidráulica, ar condicionado, limpador traseiro <br>
                 </p>
             </div>
