32668b9a822d984260e663ec1559f81543b72edc
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-06-13 14:40:46 -0300

    style: estilo card veiculo

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,16 +9,23 @@
     @foreach($veiculos as $veiculo)
 
     <div class="col-md-3">
-        <div class="veiculo-card-container mb-5">
+        <div class="veiculo-card-container mb-4">
             <h3 class="veiculo-card-title">
                 {{ $veiculo->modelo }}
             </h3>
 
             <img src="{{ $veiculo->foto }}" class="veiculo-card-image" />
 
-            <h3 class="veiculo-card-price">
-                R$ {{ $veiculo->valor }}
-            </h3>
+            <div class="row justify-content-between">
+                <div class="col-auto me-auto">
+                    <h3 class="veiculo-card-price">
+                        R$ {{ $veiculo->valor }}
+                    </h3>
+                </div>
+                <div class="col-auto">
+                    <span class="badge bg-secondary">{{ $veiculo->tipo }}</span>
+                </div>
+            </div>
 
             <div class="veiculo-card-description">
                 <p>
